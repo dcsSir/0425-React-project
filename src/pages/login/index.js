f88467462ad6422299b2295de5fe06a66bbbf541
Your Name <>
2019-08-13 18:36:40 +0800

    提交master 代码

---
+++ src/pages/login/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import { Form, Input,Icon, Button } from 'antd';
+
+export default class Login extends Component {
+    render() {
+        return <div className='login'>
+            <header cl></header>
+        </div>
+    }
+}
+
